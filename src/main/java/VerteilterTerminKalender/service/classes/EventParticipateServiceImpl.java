c4450c24b011f899999e118391e6236f0aa0f572
KerimZ <33584276+KerimZ@users.noreply.github.com>
2019-03-17 00:32:02 +0100

    added support for eventparticipate endpoint.

---
+++ src/main/java/VerteilterTerminKalender/service/classes/EventParticipateServiceImpl.java
@@ -0,0 +1,28 @@
+package VerteilterTerminKalender.service.classes;
+
+import VerteilterTerminKalender.model.interfaces.EventParticipate;
+import VerteilterTerminKalender.service.interfaces.EventParticipateService;
+
+import java.util.ArrayList;
+
+public class EventParticipateServiceImpl implements EventParticipateService{
+    @Override
+    public EventParticipate getParticipate(int participateID) {
+        return null;
+    }
+
+    @Override
+    public ArrayList getParticipants(int eventID) {
+        return null;
+    }
+
+    @Override
+    public boolean createParticipate(EventParticipate eventParticipate) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteParticipate(int participateID) {
+        return false;
+    }
+}
