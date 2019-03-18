5c2c3ba7b6ad3e3d457e704204a125d46d52a495
jogerwert <jogerwert@web.de>
2019-03-19 00:20:48 +0100

    Automatische Aktualisierung Kurzübersicht, Auslagerung von Code in Methoden

--- src/main/java/VerteilterTerminKalender/view/DayOverviewController.java
+++ src/main/java/VerteilterTerminKalender/view/DayOverviewController.java
@@ -3,6 +3,7 @@ package VerteilterTerminKalender.view;
 import VerteilterTerminKalender.MainApp;
 import VerteilterTerminKalender.model.interfaces.EventFx;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -27,10 +28,16 @@ public class DayOverviewController {
     private int dayOfMonth;
     private int month;
     private int year;
+    private ObservableList<EventFx> allEvents;
     private ObservableList<EventFx> eventsOfTheDay;
+
+    private int numberOfDisplayedEvents;
 
     public void setMainApp(MainApp mainApp){
         this.mainApp = mainApp;
+
+        this.allEvents = this.mainApp.getEventFXList();
+        addEventListener();
     }
 
     public void setup(GregorianCalendar today){
@@ -39,26 +46,74 @@ public class DayOverviewController {
         this.month = today.get(GregorianCalendar.MONTH) + 1;
         this.year = today.get(GregorianCalendar.YEAR);
 
-        ObservableList<EventFx> allEvents = mainApp.getEventFXList();
-        ListIterator<EventFx> iterator;
+        this.numberOfDisplayedEvents = 0;
 
-        Stream<EventFx> stream = allEvents.stream().filter(a -> (a.getStartTime().getValue().getYear() == this.year)
-                                    || (a.getEndTime().getValue().getYear() == this.year))
-                          .filter(a -> (a.getStartTime().getValue().getMonthValue() == this.month)
-                                    || (a.getEndTime().getValue().getMonthValue() == this.month))
-                          .filter(a -> (a.getStartTime().getValue().getDayOfMonth() == this.dayOfMonth)
-                                    || (a.getEndTime().getValue().getDayOfMonth() == this.dayOfMonth));
-
-        this.eventsOfTheDay = FXCollections.observableArrayList();
-
-        eventsOfTheDay = stream
-                .collect(FXCollections::observableArrayList, List::add, List::addAll);
-
-        iterator = eventsOfTheDay.listIterator();
+        eventsOfTheDay = filterEvents(allEvents);
+        addEventPreviewListener();
 
         labelDayOfMonth.setText(Integer.toString(dayOfMonth));
 
-        for(int i = 1; i <= 3; i++){
+        updatePreviewEvents();
+    }
+
+    private void addEventListener(){
+        allEvents.addListener(new ListChangeListener<EventFx>() {
+            @Override
+            public void onChanged(Change<? extends EventFx> c) {
+                while(c.next()){
+                    if(c.wasAdded()){
+                        for(EventFx eventFx : c.getAddedSubList()){
+                            if(checkEvent(eventFx)){
+                                eventsOfTheDay.add(eventFx);
+                            }
+                        }
+                    }
+
+                    if(c.wasRemoved()){
+                        for(EventFx eventFx : c.getRemoved()){
+                            eventsOfTheDay.remove(eventFx);
+                        }
+                    }
+
+                    if(c.wasPermutated()){
+                        eventsOfTheDay.clear();
+                        eventsOfTheDay.addAll(filterEvents(allEvents));
+                    }
+                }
+            }
+        });
+    }
+
+    private void addEventPreviewListener(){
+        eventsOfTheDay.addListener(new ListChangeListener<EventFx>() {
+            @Override
+            public void onChanged(Change<? extends EventFx> c) {
+                while(c.next()){
+                    if(c.wasAdded()){
+                        updatePreviewEvents();
+                    }
+
+                    if(c.wasRemoved()){
+                        vBoxPreviewEvents.getChildren().clear();
+                        numberOfDisplayedEvents = 0;
+                        updatePreviewEvents();
+                    }
+
+                    if(c.wasPermutated()){
+                        vBoxPreviewEvents.getChildren().clear();
+                        numberOfDisplayedEvents = 0;
+                        updatePreviewEvents();
+                    }
+                }
+            }
+        });
+    }
+
+    private void updatePreviewEvents(){
+        ListIterator<EventFx> iterator;
+        iterator = eventsOfTheDay.listIterator(numberOfDisplayedEvents);
+
+        for( ; numberOfDisplayedEvents < 3; numberOfDisplayedEvents++){
             if(iterator.hasNext()){
                 String previewEventName = iterator.next().toString();
                 Label labelPreviewEvent = new Label(previewEventName);
@@ -68,4 +123,40 @@ public class DayOverviewController {
             }
         }
     }
+
+    private boolean checkEvent(EventFx originalEvent){
+        boolean isEventOfToday = false;
+
+        if(originalEvent.getStartTime().getValue().getYear() == this.year
+                || originalEvent.getEndTime().getValue().getYear() == this.year){
+
+            if(originalEvent.getStartTime().getValue().getMonthValue() == this.month
+                    || originalEvent.getEndTime().getValue().getMonthValue() == this.month){
+
+                if(originalEvent.getStartTime().getValue().getDayOfMonth() == this.dayOfMonth
+                        || originalEvent.getEndTime().getValue().getDayOfMonth() == this.dayOfMonth){
+                    isEventOfToday = true;
+                }
+            }
+        }
+
+        return isEventOfToday;
+    }
+
+    private ObservableList<EventFx> filterEvents(ObservableList<EventFx> originalList){
+
+        ObservableList<EventFx> filteredList;
+
+        Stream<EventFx> stream = originalList.stream().filter(a -> (a.getStartTime().getValue().getYear() == this.year)
+                                    || (a.getEndTime().getValue().getYear() == this.year))
+                          .filter(a -> (a.getStartTime().getValue().getMonthValue() == this.month)
+                                    || (a.getEndTime().getValue().getMonthValue() == this.month))
+                          .filter(a -> (a.getStartTime().getValue().getDayOfMonth() == this.dayOfMonth)
+                                     || (a.getEndTime().getValue().getDayOfMonth() == this.dayOfMonth));
+
+        filteredList = stream.collect(FXCollections::observableArrayList, List::add, List::addAll);
+
+        return filteredList;
+    }
+
 }
